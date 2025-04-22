7fe15b75e7cc474c9ff2848d7d5a475b4204db41
chunchunmaru0000 <cho227675m@gmail.com>
2025-04-22 19:15:02 +0300

    first

---
+++ Key.cs
@@ -0,0 +1,32 @@
+﻿namespace VoicePress
+{
+    public enum Key
+    {
+        SPACE = 0x20,
+        L = 0x4C,
+        ESC = 0x1B,
+
+        ZERO = 0x30,
+        ONE = 0x31,
+        TWO = 0x32,
+        THREE = 0x33,
+        FOUR = 0x34,
+        FIFE = 0x35,
+        SIX = 0x36,
+        SEVEN = 0x37,
+        EIGHT = 0x38,
+        NINE = 0x39,
+    }
+
+    public enum Click
+    {
+
+    }
+
+    public enum KeyState
+    {
+        DOWN = 0,
+        EXTENDED = 1,
+        UP = 2,
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,131 @@
+﻿using NAudio.Wave;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text.Json;
+using Vosk;
+
+namespace VoicePress
+{
+    class Program
+    {
+        private static VoskRecognizer VoskRecognizer { get; set; }
+        private static Model Model { get; set; }
+        private static WaveInEvent WIE { get; set; }
+
+        [STAThread]
+        static void Main(string[] args)
+        {
+            SetupRecognition();
+
+            WIE.StartRecording();
+            Console.ReadLine();
+            WIE.StopRecording();
+
+            VoskRecognizer.Dispose();
+            Model.Dispose();
+        }
+
+        private static void SetupRecognition()
+        {
+            int deviceNum = SelectDevice();
+
+            Vosk.Vosk.SetLogLevel(0);
+            Model = new Model("C:\\myos\\vp\\ru");
+            VoskRecognizer = new VoskRecognizer(Model, 16000.0f);
+
+            WIE = new WaveInEvent()
+            {
+                DeviceNumber = deviceNum,
+                WaveFormat = new WaveFormat(16000, 1)
+            };
+            WIE.DataAvailable += DataRecognized;
+        }
+
+        private static int SelectDevice()
+        {
+            Console.WriteLine("[*] Select device:");
+            Console.WriteLine(string.Join("\n",
+                Enumerable
+                .Range(0, WaveInEvent.DeviceCount)
+                .Select(i => $"[{i}] {WaveInEvent.GetCapabilities(i).ProductName}"
+            )));
+            int deviceNum;
+            while (!int.TryParse(Console.ReadLine(), out deviceNum)) ;
+            Console.WriteLine($"[*] Selected [{WaveInEvent.GetCapabilities(deviceNum).ProductName}]");
+            return deviceNum;
+        }
+
+        private static void DataRecognized(object s, WaveInEventArgs e) 
+        {
+            if (!VoskRecognizer.AcceptWaveform(e.Buffer, e.BytesRecorded))
+                return;
+                /*
+                string result = VoskRecognizer.PartialResult();
+                Console.WriteLine($"[r] Recognized partial result: \n{result}");
+
+                string text =
+                    JsonDocument.Parse(result)
+                    .RootElement.GetProperty("partial")
+                    .GetString();
+                if (!string.IsNullOrWhiteSpace(text))
+                    Console.WriteLine($"[P] Partial text\n{text}");
+                    */
+
+            string text =
+                JsonDocument
+                .Parse(VoskRecognizer.Result())
+                .RootElement
+                .GetProperty("text")
+                .GetString();
+
+            Console.WriteLine($"[T] Text\n{text}");
+            TryTextOnCommands(text);
+        }
+
+        [DllImport("User32.dll")]
+        private static extern void keybd_event(byte bVk, byte bScan, uint dwFlags, ulong dwExtraInfo);
+
+        private static void PressKey(Key key) => PressKey((byte)key);
+
+        private static void PressKey(byte key)
+        {
+            keybd_event(key, 0, (uint)KeyState.DOWN, 0);
+            keybd_event(key, 0, (uint)KeyState.UP, 0);
+        }
+
+        private static Dictionary<string, Key> KeyCommands { get; set; } = new Dictionary<string, Key>() {
+            { "ноль", Key.ZERO },
+            { "один", Key.ONE },
+            { "два", Key.TWO },
+            { "три", Key.THREE },
+            { "четыре", Key.FOUR },
+            { "пять", Key.FIFE },
+            { "шесь", Key.SIX },
+            { "семь", Key.SEVEN },
+            { "восемь", Key.EIGHT },
+            { "девять", Key.NINE },
+
+            { "центр", Key.SPACE },
+            { "энтер", Key.SPACE },
+            { "пробел", Key.SPACE },
+
+            { "текст", Key.L },
+            { "история", Key.L },
+            { "лог", Key.L },
+
+            { "выход", Key.ESC },
+            { "выйти", Key.ESC },
+        };
+
+        private static void TryTextOnCommands(string text)
+        {
+            string[] words = text.ToLower().Split(' ');
+            foreach(string word in words) {
+                if (KeyCommands.ContainsKey(word))
+                    PressKey(KeyCommands[word]);
+            }
+        }
+    }
+}
